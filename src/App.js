51fa0b823a7b4fecc572f9f38a3bf6f1c16c2fff
giau.nguyen <giau.nguyen@executionlab.asia>
2021-06-07 18:32:02 +0700

    simple agora react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from 'pages/Home'
-import Meeting from 'pages/Meeting'
+import Meeting from 'pages/MeetingDemo'
 import './App.css';
 
 function App() {
